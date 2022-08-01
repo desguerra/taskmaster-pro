f41734a95c70877fab6b46d9c3454a970e16baed
desguerra <ddesguerra@ucdavis.edu>
2022-07-31 22:43:55 -0700

    save task in correct spot after drag and drop

--- assets/js/script.js
+++ assets/js/script.js
@@ -142,8 +142,81 @@ $(".list-group").on("blur", "input[type='text']", function() {
   
     // replace input with span element
     $(this).replaceWith(taskSpan);
-  });
+});
 
+// convert .card .list-group into a sortable
+$(".card .list-group").sortable({
+    connectWith: $(".card .list-group"),
+    scroll: false,
+    tolerance: "pointer",
+    // create a copy of the dragged element and move the 
+    // copy instead of the original. This is necessary to 
+    // prevent click events from accidentally triggering on 
+    // the original element.
+    helper: "clone",
+    // trigger once for all connected lists as soon as 
+    // dragging starts and stops
+    activate: function(event) {
+        console.log("activate", this);
+    },
+    deactivate: function(event) {
+        console.log("deactivate", this);
+    },
+    // when a dragged item enters or leaves a connected list
+    over: function(event) {
+        console.log("over", event.target);
+    },
+    out: function(event) {
+        console.log("out", event.target);
+    },
+    // triggers when the contents of a list have changed
+    update: function(event) {
+        // array to store the task data in
+        var tempArr = [];
+        // loop over current set of children in sortable list
+        $(this).children().each(function() {
+            var text = $(this)
+                .find("p")
+                .text()
+                .trim();
+
+            var date = $(this)
+                .find("span")
+                .text()
+                .trim();
+
+            // add task data to the temp array as an object
+            tempArr.push({
+                text: text,
+                date: date
+            });
+        });
+
+        // trim down list's ID to match object property
+        var arrName = $(this)
+        .attr("id")
+        .replace("list-", "");
+
+        // update array on tasks object and save
+        tasks[arrName] = tempArr;
+        saveTasks();
+    }
+});
+
+// convert trash into droppable
+$("#trash").droppable({
+    accept: ".card .list-group-item",
+    tolerance: "touch",
+    drop: function(event, ui) {
+      console.log("drop");
+    },
+    over: function(event, ui) {
+      console.log("over");
+    },
+    out: function(event, ui) {
+      console.log("out");
+    }
+});
 
 
 // modal was triggered
