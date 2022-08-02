721087b105da26ce299943ef9f3ccbdd32cbae84
desguerra <ddesguerra@ucdavis.edu>
2022-08-01 17:09:27 -0700

    UX enhancements

--- assets/js/script.js
+++ assets/js/script.js
@@ -196,17 +196,19 @@ $(".card .list-group").sortable({
     // trigger once for all connected lists as soon as 
     // dragging starts and stops
     activate: function(event) {
-        console.log("activate", this);
-    },
+        $(this).addClass("dropover");
+        $(".bottom-trash").addClass("bottom-trash-drag");
+;    },
     deactivate: function(event) {
-        console.log("deactivate", this);
+        $(this).removeClass("dropover");
+        $(".bottom-trash").removeClass("bottom-trash-drag");
     },
     // when a dragged item enters or leaves a connected list
     over: function(event) {
-        console.log("over", event.target);
+        $(event.target).addClass("dropover-active");
     },
     out: function(event) {
-        console.log("out", event.target);
+        $(event.target).removeClass("dropover-active");
     },
     // triggers when the contents of a list have changed
     update: function(event) {
@@ -248,12 +250,15 @@ $("#trash").droppable({
     tolerance: "touch",
     drop: function(event, ui) {
         ui.draggable.remove();
+        $(".bottom-trash").removeClass("bottom-trash-active");
     },
     over: function(event, ui) {
         console.log("over");
+        $(".bottom-trash").addClass("bottom-trash-active");
     },
     out: function(event, ui) {
         console.log("out");
+        $(".bottom-trash").removeClass("bottom-trash-active");
     }
 });
 
@@ -276,7 +281,7 @@ $("#task-form-modal").on("shown.bs.modal", function() {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function() {
+$("#task-form-modal .btn-save").click(function() {
     // get form values
     var taskText = $("#modalTaskDescription").val();
     var taskDate = $("#modalDueDate").val();
@@ -309,4 +314,9 @@ $("#remove-tasks").on("click", function() {
 // load tasks for the first time
 loadTasks();
 
-
+// audit tasks every 30 mins
+setInterval(function () {
+    $(".card .list-group-item").each(function(index, el) {
+      auditTask(el);
+    });
+}, 1800000);
